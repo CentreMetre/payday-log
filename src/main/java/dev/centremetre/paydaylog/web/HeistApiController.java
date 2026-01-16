ea4f93577be280f02b92e8a9a021a32f9038040a
CentreMetre <luckyminer01@gmail.com>
2026-01-16 10:11:32 +0000

    Change heist web class.
    Edit endpoints to prepend /api/heists to all methods using @RequestMapping.
    Fix createNewHeist endpoint.
    Edit getAllHeists endpoint.

--- src/main/java/dev/centremetre/paydaylog/web/HeistApiController.java
+++ src/main/java/dev/centremetre/paydaylog/web/HeistApiController.java
@@ -2,23 +2,19 @@ package dev.centremetre.paydaylog.web;
 
 import dev.centremetre.paydaylog.dto.HeistCreateDto;
 import dev.centremetre.paydaylog.model.Heist;
-import dev.centremetre.paydaylog.service.CompletedHeistService;
 import dev.centremetre.paydaylog.service.HeistService;
-import org.apache.coyote.Response;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/api/heists")
 public class HeistApiController
 {
     private final HeistService heistService;
 
-    public HeistApiController(HeistService heistService, CompletedHeistService completedHeistService)
+    public HeistApiController(HeistService heistService)
     {
         this.heistService = heistService;
     }
@@ -27,13 +23,21 @@ public class HeistApiController
      * Get all heists from the DB.
      * @return A response entity that has a list of heists in the game.
      */
-    @GetMapping("/heists/game")
+    @GetMapping("")
     public ResponseEntity<List<Heist>> getAllHeists()
     {
         return ResponseEntity.ok(heistService.getAllHeists());
     }
 
-    @PostMapping
+    /**
+     * JSON body should look like this.
+     * {
+     *     heistName: "Heist Name"
+     * }
+     * @param heistName The name of the heist.
+     * @return The created heist object in the DB.
+     */
+    @PostMapping("/create")
     public ResponseEntity<Heist> createNewHeist(@RequestBody HeistCreateDto heistName)
     {
         return ResponseEntity.ok(heistService.createHeist(heistName.getHeistName()));
