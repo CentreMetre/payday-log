413ba50df40f418e49474424628e51b2fcfede32
CentreMetre <luckyminer01@gmail.com>
2026-02-07 17:48:13 +0000

    Add type for completed heist object sent from server.

--- frontend/typescript/models/completed-heist.ts
+++ frontend/typescript/models/completed-heist.ts
@@ -26,7 +26,11 @@ export type CompletedHeist = {
     notes: string;
 }
 
-export type CompletedHeistDefaultRowShape = Omit<CompletedHeist, 'heistFinishStateId' | 'heistId' | 'difficultyId' | 'completedAt'> & {
+export type CompletedHeistResponseOmitObjectIds =
+    Omit<CompletedHeist, 'heistFinishStateId' | 'heistId' | 'difficultyId'>
+
+export type CompletedHeistDefaultRowShape =
+    Omit<CompletedHeist, 'heistFinishStateId' | 'heistId' | 'difficultyId' | 'completedAt'> & {
     completedAt: string; // Since the completedAt is sent in JSON as "2026-01-23T23:17:30.183",
     // it needs to be a string type here, not a date type.
     // Converting it to a date would make it a human-readable Date string, but I want it as an ISO string.
