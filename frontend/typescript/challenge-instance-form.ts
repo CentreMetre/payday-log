41a882eb86c7be835f10503fb96e97a1592f0fa4
CentreMetre <luckyminer01@gmail.com>
2026-02-07 17:49:11 +0000

    Add code for challenge form.

--- frontend/typescript/challenge-instance-form.ts
+++ frontend/typescript/challenge-instance-form.ts
@@ -1,5 +1,6 @@
 import {createNewDateStringForForm} from "./util.js";
 import type {Challenge} from "models/challenge";
+import type {CompletedHeistResponseOmitObjectIds} from "./models/completed-heist";
 
 const challengeInstanceNewChallengeButtonEl: HTMLButtonElement =
     document.getElementById("challenge-instance-new-challenge-instance-button") as HTMLButtonElement;
@@ -11,6 +12,8 @@ challengeInstanceChallengeInputEl.addEventListener("input", () => {
     challengeInstanceChallengeInputEl.title = challengeInstanceChallengeInputEl.value;
 })
 
+// challengeInstanceChallengeInputEl.append(createToolTip("Hover over the input box to see full contents."))
+
 
 const challengesDataListEl: HTMLDataListElement =
     document.getElementById("challenge-data-list") as HTMLDataListElement;
@@ -21,27 +24,77 @@ const challengeInstanceIsCompleteInputEl: HTMLInputElement =
 const challengeInstanceNotesInputEl: HTMLInputElement =
     document.getElementById("challenge-instance-notes-input") as HTMLInputElement;
 
+// ------------
+// Time buttons
+// ------------
 const challengeInstanceDateTimeCompletedInputEl: HTMLInputElement =
     document.getElementById("challenge-instance-completed-at-input") as HTMLInputElement;
 
-const challengeInstanceDateTimeCompletedInsertTimeButtonEl: HTMLButtonElement =
-    document.getElementById("challenge-instance-completed-at-insert-time-latest-heist-completed-button") as HTMLButtonElement;
-
-challengeInstanceDateTimeCompletedInsertTimeButtonEl.addEventListener("click", setCompletedAndDateCompletedTime)
-
 const challengeInstanceDateTimeCompletedResetTimeButtonEl: HTMLButtonElement =
-    document.getElementById("challenge-instance-completed-at-reset-time-button") as HTMLButtonElement;
+    document.getElementById("challenge-instance-complete-at-reset-time-button") as
+        HTMLButtonElement;
+
+challengeInstanceDateTimeCompletedResetTimeButtonEl.addEventListener("click", setCompletedAndDateCompletedTime)
+
+const challengeInstanceDateTimeCompletedInsertLatestCompletedHeistButtonEl: HTMLButtonElement =
+    document.getElementById("challenge-instance-completed-at-insert-time-latest-heist-completed-button") as
+        HTMLButtonElement;
+
+challengeInstanceDateTimeCompletedInsertLatestCompletedHeistButtonEl.addEventListener("click", setLatestHeistCompletedDateTime)
 
 
 const challengeInstanceSubmitButtonEl: HTMLButtonElement =
     document.getElementById("challenge-instance-submit-button") as HTMLButtonElement;
 
+challengeInstanceSubmitButtonEl.addEventListener("click", submitChallengeInstance)
+
+
+const challengeInstanceFormMessagePEl: HTMLParagraphElement =
+    document.getElementById("challenge-instance-form-message") as HTMLParagraphElement;
+
+
+
 /**
- * Map with the challenge name as the key, id as its value.
+ * Map with the challenge name as the key, ID as its value.
  */
 const challengeNameToId: Map<string, number> = new Map();
 
-let dateTime: string;
+let formDateTimeComplete: string | undefined = undefined;
+
+function setCompletedAndDateCompletedTime() {
+    // challengeInstanceIsCompleteInputEl.checked = true;
+    // challengeInstanceDateTimeCompletedInputEl.value = createNewDateStringForForm();
+}
+
+function validateFormData(): boolean {
+    let valid = true;
+
+
+
+    return false;
+}
+
+async function setLatestHeistCompletedDateTime() {
+    debugger
+    const response = await fetch("/api/completed-heists/latest");
+
+    const body: CompletedHeistResponseOmitObjectIds = await response.json();
+    const dateTimeWithZ = new Date(body.completedAt).toISOString();
+    const correctDateTime = dateTimeWithZ.replace("Z", "");
+
+    if (challengeInstanceDateTimeCompletedInputEl.value == correctDateTime)
+    {
+        return;
+    }
+    challengeInstanceDateTimeCompletedInputEl.value = correctDateTime;
+}
+
+
+function submitChallengeInstance() {
+    validateFormData()
+}
+
+// Setup
 
 async function populateChallengesList() {
     const response = await fetch("/api/challenges")
@@ -56,16 +109,4 @@ async function populateChallengesList() {
     }
 }
 
-function setCompletedAndDateCompletedTime() {
-    challengeInstanceIsCompleteInputEl.checked = true;
-    challengeInstanceDateTimeCompletedInputEl.value = createNewDateStringForForm();
-}
-
-function validateFormData() {
-
-}
-
-if (!challengeInstanceDateTimeCompletedInputEl.value) {
-
-    console.log(false)
-}
+await populateChallengesList()
