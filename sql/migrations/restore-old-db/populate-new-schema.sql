2aa503cf03f25e96278fd0d64ebacbcd100dcbe6
CentreMetre <luckyminer01@gmail.com>
2026-02-12 11:14:26 +0000

    Improve migration.
    Add new script for adding heists and difficulties.
    Change database name to variable.

---
+++ sql/migrations/restore-old-db/populate-new-schema.sql
@@ -0,0 +1,32 @@
+BEGIN;
+
+INSERT INTO HEISTS (name) VALUES
+    ('No Rest For The Wicked'),
+    ('Road Rage'),
+    ('Dirty Ice'),
+    ('Rock The Cradle'),
+    ('Under The Surphaze'),
+    ('Gold & Sharke'),
+    ('99 Boxes'),
+    ('Touch The Sky'),
+    ('Cook Off'),
+    ('Turbid Station'),
+    ('Syntax Error'),
+    ('Boys In Blue'),
+    ('Houston Breakout'),
+    ('Diamond District'),
+    ('Fear & Greed'),
+    ('First World Bank'),
+    ('Party Powder'),
+    ('Bank Withdrawal'),
+    ('Search And Seizure'),
+    ('Delivery Charge'),
+    ('Shopping Spree');
+
+INSERT INTO DIFFICULTIES (DIFFICULTY) VALUES
+    ('Normal'),
+    ('Hard'),
+    ('Very Hard'),
+    ('Overkill');
+
+COMMIT;
